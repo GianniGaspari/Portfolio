680fdebb3e452ae7fb1e3c5ccc6282ae339e85e8
Gianni Gaspari <gianni.gaspari.s@gmail.com>
2023-02-09 03:26:54 -0300

    Update scroll.js

--- Portfolio/assets/js/scroll.js
+++ Portfolio/assets/js/scroll.js
@@ -8,5 +8,4 @@ sr.reveal('.sobre-mim', {duration : 3000, origin:'right'})
 sr.reveal('.projetos-itens', {duration : 3000, origin:'bottom'})
 sr.reveal('.project', {duration : 3000, origin:'bottom'})
 sr.reveal('.github-link', {duration : 3000, origin:'bottom'})
-sr.reveal('.conteiner-skills', {duration : 3000, origin:'left'})
-sr.reveal('.conteiner-descricao', {duration : 3000, origin:'right'})
+

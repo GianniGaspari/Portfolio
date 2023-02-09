7b19b1844253d5165818018eb5457a7287b4dd5b
Gianni Gaspari <gianni.gaspari.s@gmail.com>
2023-02-09 03:50:17 -0300

    scroll btn-ver-mais

--- Portfolio/assets/js/scroll.js
+++ Portfolio/assets/js/scroll.js
@@ -8,4 +8,6 @@ sr.reveal('.sobre-mim', {duration : 3000, origin:'right'})
 sr.reveal('.projetos-itens', {duration : 3000, origin:'bottom'})
 sr.reveal('.project', {duration : 3000, origin:'bottom'})
 sr.reveal('.github-link', {duration : 3000, origin:'bottom'})
-
+sr.reveal('.conteiner-skills', {duration : 3000, origin:'left'})
+sr.reveal('.conteiner-descricao', {duration : 3000, origin:'right'})
+sr.reveal('.btn-ver', {duration : 3000, origin:'bottom'})
